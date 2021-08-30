7a6057baf720559ec591552572960185ebe0e430
Manuel Torres <manueljosetorres02@gmail.com>
2021-08-29 23:51:51 -0500

    setting up site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import React, { useState } from 'react';
+import TimeInput from "./components/TimeInput"
 import './App.css';
 
 function App() {
+
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">      </header>
         <div className="container">
         <h1> The Timer</h1>
         {/* SET HOURS, MINS   */}
+        <TimeInput/>
         {/* TIMER SECTION  */}
         {/* START/STOP  */}
       </div>
---
+++ src/components/TimeInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TimeInput= ()=> {
+    return(
+    <form>
+        <label> hello</label>
+    </form>
+    )
+}
+
+export default TimeInput
