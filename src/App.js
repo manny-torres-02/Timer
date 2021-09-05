3ca9a4ed28caaa7bf7d28f6cf7cb5ee50280b105
Manuel Torres <manueljosetorres02@gmail.com>
2021-09-05 10:46:47 -0500

    Work on useEffect hook with SEconds

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 // import TimeInput from "./components/TimeInput"
 import './App.css';
 
 function App() {
 
-  const [minutes, setMinutes] = useState(10)
+  const [minutes, setMinutes] = useState(2)
+  const [seconds, setSeconds] = useState(0)
   const [isActive, setIsActive] = useState(false)
+  const [total, setTotal] = useState((minutes * 60))
 
-  function increment (minutes) { 
-    console.log(1)
-    setMinutes(prevMinutes => prevMinutes+1)
+  function increment() {
+    setMinutes(prevMinutes => prevMinutes + 1)
   }
 
-  function decrement (minutes) {
-    console.log(1)
-    setMinutes(prevMinutes => prevMinutes -1)
+  function decrement() {
+    setMinutes(prevMinutes => prevMinutes - 1)
   }
 
+  function start() {
+    setIsActive(prevIsActive => !prevIsActive)
+    console.log(isActive)
+  }
+
+  useEffect(() => {
+    
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        console.log("rendered")
+        setTotal(prevTotal => prevTotal - 1);
+        // console.log(minutes)
+        if(total%60===0) {
+          console.log("thisworks") 
+          setMinutes(prevMinutes => prevMinutes -1)
+        }
+        // run the total so that we can subtract seconds
+        // once the value hits 0- revert back to 59 
+        // continue to decrease until minutes turns to 0
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, total]);
+
+  // function calculateTimeLeft() {
+
+  // }
   return (
     <div className="App">
       <header className="App-header">      </header>
-        <div className="container">
+      <div className="container">
         <h1> The Timer</h1>
         {/* SET HOURS, MINS   */}
         <div className="input-time">
-        <button onClick={increment}>+</button>
-        <span>{minutes}</span>
-        <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+      
+          {/* <span>{seconds}</span> */}
+          <button onClick={decrement}>-</button>
         </div>
         {/* TIMER SECTION  */}
         <div className="display">
-        {/* <p>{runningMinutes}</p> */}
-          </div>        
+        <h2> Total Seconds </h2>
+          <span>{total}</span>
+          {/* <p>{runningMinutes}</p> */}
+        </div>
+        <h2> time </h2> 
+        <span>{minutes} : {seconds}</span>
         {/* START/STOP  */}
         <button onClick={start}> Start</button>
       </div>
